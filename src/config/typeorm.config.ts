80f00527c7e882ace246d552e3bcd470c7ad210a
Roman Kiss <romiki44@hotmail.com>
2020-01-24 19:20:15 +0100

    section08 env variables done

--- src/config/typeorm.config.ts
+++ src/config/typeorm.config.ts
@@ -1,12 +1,15 @@
 import { TypeOrmModuleOptions } from '@nestjs/typeorm';
+import * as config from 'config';
+
+const dbConfig=config.get('db');
 
 export const typeOrmConfig: TypeOrmModuleOptions ={
-  type: 'postgres',
-  host: 'localhost',
-  port: 5432,
-  username: 'postgres',
-  password: 'rokisun',
-  database: 'taskmanagment',
+  type: dbConfig.type,  //'postgres',
+  host: process.env.RDS_HOSTNAME || dbConfig.host, //'localhost',
+  port: process.env.RDS_PORT || dbConfig.port,  //5432,
+  username: process.env.RDS_USERNAME || dbConfig.username, //'postgres',
+  password: process.env.RDS_PASSWORD || dbConfig.password, //'postgres',
+  database: process.env.RDS_DB_NAME || dbConfig.database, //'taskmanagment',
   entities: [__dirname+'/../**/*.entity.{js, ts}'],
-  synchronize: true
+  synchronize: process.env.TYPEORM_SYNC || dbConfig.synchronize //true
 }
